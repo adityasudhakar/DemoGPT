f92f3633e5f93c5eba50885ac2799d714d47fe7e
adityasudhakar <aditya.sudhakar@gmail.com>
2024-03-29 10:31:17 -0700

    changed prompt text

--- test.py
+++ test.py
@@ -5,6 +5,6 @@ from demogpt.chains.chains import Chains
 
 Chains.setLlm("gpt-3.5-turbo")
 
-instruction = "Create an app that can say the number of columns, rows, the name of columns from csv file"
+instruction = "Create an app that says the number of columns, rows, the name of columns from csv file"
 res = Chains.appType(instruction=instruction)
 pprint(res)
